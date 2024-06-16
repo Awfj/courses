db942a4fae66998e74aea32b30939558d5119f3a
AWFJ <awfj837@gmail.com>
2024-06-16 18:55:04 +0300

    add module [8]

---
+++ JS Core Advanced/[8] Promise/fetchUrls.js
@@ -0,0 +1,27 @@
+function fetchUrls(urls) {
+  const promises = urls
+      .map(url => fetch(url)
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+              }
+              return response.json();
+          }));
+  return Promise.allSettled(promises);
+}
+
+const urls = [
+  'https://jsonplaceholder.typicode.com/posts/1',
+  'https://jsonplaceholder.typicode.com/posts/2',
+  'https://jsonplaceholder.typicode.com/posts/3'
+];
+
+fetchUrls(urls).then(contents => {
+  contents.forEach(content => {
+      if (content.status === 'fulfilled') {
+          console.log(content.value);
+      } else {
+          console.log(content.reason);
+      }
+  });
+});
